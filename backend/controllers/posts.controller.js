95f07788db691c61f5717413bfed3bf627ff05ea
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-01 23:57:46 +0530

    register controller

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -3,3 +3,12 @@
 export const activeCheck = async(req,res)=>{
     res.status(200).json({message : "Running!"});
 }
+
+
+export const register = async (req,res)=>{
+        try{
+
+        }catch(err){
+            return res.status(500).json({message : err.message});
+        }
+}
