9165b3094149faf058f5c3eaaeff66f36d1d3176
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-02 00:01:40 +0530

    register controller

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,4 +1,4 @@
-
+import User from "../models/user.model.js"
 
 export const activeCheck = async(req,res)=>{
     res.status(200).json({message : "Running!"});
@@ -7,7 +7,13 @@ export const activeCheck = async(req,res)=>{
 
 export const register = async (req,res)=>{
         try{
-
+            const {name, username,email,password} = req.body;
+            if(!name ||!username ||!email||!password){
+                res.status(400).json({message : "All fields are required!"});
+                return;
+            }
+            const user = await User.findOne({email});
+            
         }catch(err){
             return res.status(500).json({message : err.message});
         }
