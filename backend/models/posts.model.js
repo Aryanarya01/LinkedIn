1327b54e470bedf3751015ba6b09ad4e04800a77
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-01 22:50:56 +0530

    postsmodel

---
+++ backend/models/posts.model.js
@@ -0,0 +1,22 @@
+
+import mongoose from "mongoose"
+const PostSchema = mongoose.Schema({
+    userId :{
+
+    },
+    body : {
+
+    },
+    likes : {
+
+    },
+    createdAt : {
+
+    },
+    updatedAt : {
+        
+    },
+    media : {
+
+    },
+})
