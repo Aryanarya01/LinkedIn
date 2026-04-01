f88142b56cbe2eee9c4a1ca9e7769bf3589a8933
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-01 22:34:48 +0530

    route

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,19 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+
+app.use(cors());
+
+const port = 9080;
+const start = async ()=>{
+    const connectDB = await mongoose.connect("mongodb+srv://aryanarya01:aryan5555@linkedin.mdrgeum.mongodb.net/?appName=LinkedIn")
+    app.listen(port,()=>{
+        console.log(`App is listining to port ${port}`);
+    })
+}
+
+start();
