d2d4fdd5dae636a44b096c15b9fa529c8fc3c1e9
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-02 15:19:54 +0530

    multer

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,7 +3,14 @@ import { login, register } from "../controllers/user.controller.js";
 import multer from "multer";
 const router = Router();
 
-
+const storage = multer.diskStorage({
+    destination : (req,File,cb)=>{
+        cb(null,'uploads/')
+    },
+    filename:(req,File,cb)=>{
+        cb(null,File.originalname)
+    },
+})
 
 router.route("/register").post(register);
 router.route("/login").post(login)
