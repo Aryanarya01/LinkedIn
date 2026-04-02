5476a1b5a6ad9d210db50693587afe8b01a6b50d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-02 15:17:10 +0530

    multer

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { login, register } from "../controllers/user.controller.js";
+import multer from "multer";
 const router = Router();
+
+
 
 router.route("/register").post(register);
 router.route("/login").post(login)
