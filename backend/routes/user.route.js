ba6975ac5dcc987a803e58f5f133f33c42f35f81
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-08 21:38:36 +0530

    downloadResume completed

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllUserProfile, getUserAndProfile, login, register, updateProfileData, updateUserProfile, uploadProfilePicture } from "../controllers/user.controller.js";
+import { downloadProfile, getAllUserProfile, getUserAndProfile, login, register, updateProfileData, updateUserProfile, uploadProfilePicture } from "../controllers/user.controller.js";
 import multer from "multer";
 const router = Router();
 
@@ -20,5 +20,5 @@ router.route("/user_update").post(updateUserProfile)
 router.route("/get_user_and_Profile").get(getUserAndProfile)
 router.route("/update_profile_data").post(updateProfileData);
 router.route("/user/get_all_users").get(getAllUserProfile);
-router.route("/user/download_resume")
+router.route("/user/download_resume").get(downloadProfile);
 export default router;
