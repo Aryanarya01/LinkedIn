c4e021c15e8238d936ddd84e4f9fa800cd1b7a63
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-09 22:04:11 +0530

    getAllPosts

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -29,6 +29,8 @@ export const activeCheck = async(req,res)=>{
 
  export const getAllPosts = async (req,res)=>{
     try{
-        
+
+    }catch(err){
+        return res.status(500).json({message : err.message})
     }
  }
