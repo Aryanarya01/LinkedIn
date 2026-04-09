ef8603604653eff7b687af5d98e71c37e4bc36a7
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-09 07:31:51 +0530

    setting up multer for posts

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { activeCheck } from "../controllers/posts.controller.js";
-
+import multer from "multer"
 const router = Router();
 
-
+const storage = multer.diskStorage({
+    destination : (req,file,cb)=>{
+        cb(null,'uploads/')
+    },
+    
+})
 router.route('/').get(activeCheck)
 
 export default router;
