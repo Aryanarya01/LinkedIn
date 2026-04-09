20cac5e5c8ceff43091a8bfaec8dad214e2665bc
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-09 22:58:42 +0530

    post comment completed

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,92 +1,92 @@
- import User from "../models/user.model.js"
- import Post from "../models/posts.model.js"
+import User from "../models/user.model.js";
+import Post from "../models/posts.model.js";
 import Comment from "../models/comments.model.js";
 
+export const activeCheck = async (req, res) => {
+  res.status(200).json({ message: "Running!" });
+};
 
-
-export const activeCheck = async(req,res)=>{
-    res.status(200).json({message : "Running!"});
-}
-
- export const createPost = async (req,res)=>{
-    const {token} = req.body;
-    try{
-        const user = await User.findOne({token});
-        if(!user){
-            res.status(404).json({message : "User not found!"});
-            return;
-        }
-
-        const post = new Post({
-            userId : user._id,
-            body : req.body.body,
-            media : req.file != undefined ? req.file.filename : "",
-            fileType : req.file != undefined ? req.file.mimetype.split("/")[1] : "",
-        })
-        await post.save();
-        return res.status(200).json({message : "Post Created!"});
-    }catch(err){
-        return res.status(500).json({message : err.message});
+export const createPost = async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = await User.findOne({ token });
+    if (!user) {
+      res.status(404).json({ message: "User not found!" });
+      return;
     }
 
- }
+    const post = new Post({
+      userId: user._id,
+      body: req.body.body,
+      media: req.file != undefined ? req.file.filename : "",
+      fileType: req.file != undefined ? req.file.mimetype.split("/")[1] : "",
+    });
+    await post.save();
+    return res.status(200).json({ message: "Post Created!" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
 
- export const getAllPosts = async (req,res)=>{
-    try{
-        const posts = await Post.find().populate('userId','name username email profilePicture');
-        return res.json({posts})
-    }catch(err){
-        return res.status(500).json({message : err.message})
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().populate(
+      "userId",
+      "name username email profilePicture",
+    );
+    return res.json({ posts });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const { token, post_id } = req.body;
+  try {
+    const user = await User.findOne({ token: token }).select("_id");
+    if (!user) {
+      res.status(404).json({ message: "User not found!" });
+      return;
     }
- }
-
-
- export const deletePost = async (req,res)=>{
-        const {token, post_id} = req.body; 
-    try{
-        const user = await User.findOne({token : token}).select("_id");
-        if(!user){
-            res.status(404).json({message : "User not found!"});
-            return;
-        }
-        const post = await Post.findOne({_id : post_id});
-        if(!post){
-            res.status(404).json({message : "Post not found!"});
-            return;
-        }
-
-        if(post.userId.toString() !== user._id.toString()){
-            res.status(401).json({message : "Unauthorized"});
-            return;
-        }
-        await Post.deleteOne({_id : post._id})// check
-    }catch(err){
-        return res.status(500).json({message : err.message});
+    const post = await Post.findOne({ _id: post_id });
+    if (!post) {
+      res.status(404).json({ message: "Post not found!" });
+      return;
     }
- }
 
-
- export const commentPost  = async(req,res)=>{
-    const {token, post_id, commentBody} = req.body;
-
-    try{
-        const user = await User.findOne({token : token}).select("_id");
-        if(!user){
-            res.status(404).json({message : "User not found!"});
-            return;
-        }
-
-        const post = await Post.findOne({_id : post_id});
-        if(!post){
-            res.status(404).json({message : "Post not found!"});
-            return;
-        }
-        const comment = new Comment({
-            userId : use
-        })
-
-    }catch(err){
-        return res.status(500).json({message : err.message});
-
+    if (post.userId.toString() !== user._id.toString()) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
     }
- }
+    await Post.deleteOne({ _id: post._id }); // check
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+export const commentPost = async (req, res) => {
+  const { token, post_id, commentBody } = req.body;
+
+  try {
+    const user = await User.findOne({ token: token }).select("_id");
+    if (!user) {
+      res.status(404).json({ message: "User not found!" });
+      return;
+    }
+
+    const post = await Post.findOne({ _id: post_id });
+    if (!post) {
+      res.status(404).json({ message: "Post not found!" });
+      return;
+    }
+    const comment = new Comment({
+      userId: user._id,
+      postId: post_id,
+      body: commentBody,
+    });
+    await comment.save();
+    res.status(200).json({ message: "Comment Added!" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
