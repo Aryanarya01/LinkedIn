2430dd36a2e00bcf39ae2ba19977622053671fee
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-09 22:25:46 +0530

    deletePost

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -40,7 +40,11 @@ export const activeCheck = async(req,res)=>{
  export const deletePost = async (req,res)=>{
         const {token, post_id} = req.body; 
     try{
-        
+        const user = await User.findOne({token : token}).select("_id");
+        if(!user){
+            res.status(404).json({message : "User not found!"});
+            return;
+        }
     }catch(err){
         return res.status(500).json({message : err.message});
     }
