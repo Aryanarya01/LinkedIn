183b18b42a0f2ab5f8fe12144b3e15259d06817b
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-09 07:36:47 +0530

    creating post

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -3,4 +3,11 @@ export const activeCheck = async(req,res)=>{
     res.status(200).json({message : "Running!"});
 }
 
- 
+ export const createPost = async (req,res)=>{
+    try{
+
+    }catch(err){
+        
+    }
+
+ }
