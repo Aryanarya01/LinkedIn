a8edfc983b67184d43ce4c6a64dfbc1bf2addf69
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-09 07:39:17 +0530

    creating post

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,11 +1,16 @@
- 
+ import User from "../models/user.model.js"
 export const activeCheck = async(req,res)=>{
     res.status(200).json({message : "Running!"});
 }
 
  export const createPost = async (req,res)=>{
+    const {token} = req.body;
     try{
-
+        const user = await User.findOne({token});
+        if(!user){
+            res.status(404).json({message : "User not found!"});
+            return;
+        }
     }catch(err){
         return res.status(500).json({message : err.message});
     }
