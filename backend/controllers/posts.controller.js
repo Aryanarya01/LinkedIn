6e03ed7b04c5d8a02f75ee9f6308231f04565542
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-09 23:39:49 +0530

    increment likes

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -129,3 +129,16 @@ export const delete_comment_of_user = async (req,res)=>{
         return res.status(500).json({message : err.message});
     }
 }
+
+
+export const increament_likes = async(req,res)=>{
+    const {post_id} = req.body;
+    try{
+        const post = await Post.findOne({_id : post_id});
+        if(!post){
+            return res.status(404).json({message : "Post not found!"});
+        }
+    }catch(err){
+        return res.status(500).json({message : err.message})
+    }
+}
