220568977dfc75cae63dcacc078f67c69c0d1c11
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-10 22:23:40 +0530

    wrapped in provider

--- frontend/.next/dev/static/chunks/pages/_app.js
+++ frontend/.next/dev/static/chunks/pages/_app.js
@@ -1,12 +1,13 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/node_modules_next_dist_compiled_0o6l_m6._.js",
+  "static/chunks/node_modules_next_dist_compiled_0ywsb3_._.js",
   "static/chunks/node_modules_next_dist_shared_lib_0~pg0mt._.js",
   "static/chunks/node_modules_next_dist_client_0pe1dg-._.js",
   "static/chunks/node_modules_next_dist_0k8zr~w._.js",
   "static/chunks/node_modules_react-dom_0bruynb._.js",
-  "static/chunks/node_modules_11hu_-9._.js",
-  "static/chunks/[root-of-the-server]__0k9~j1z._.js",
+  "static/chunks/node_modules_axios_lib_07q4_l1._.js",
+  "static/chunks/node_modules_07g-dj.._.js",
+  "static/chunks/[root-of-the-server]__0dep711._.js",
   "static/chunks/src_styles_globals_0fuav90.css",
   "static/chunks/src_pages__app_07xvfw~._.js",
-  "static/chunks/turbopack-src_pages__app_0sgxe54._.js"
+  "static/chunks/turbopack-src_pages__app_0s2qqw.._.js"
 ])
--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,8 +1,11 @@
+import { Store } from "@/config/redux/store";
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
-   <Provider>
-    return <Component {...pageProps} />;
+return(
+     <Provider store={Store}>
+    <Component {...pageProps} />;
    </Provider>
+)
 }
