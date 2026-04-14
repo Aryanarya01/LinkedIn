54ef025adb9287466bb54af0e0b3ddbbccd89b8e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-14 18:50:39 +0530

    Navbar router

--- frontend/src/Components/Navbar/index.jsx
+++ frontend/src/Components/Navbar/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import styles from "./styles.module.css"
+import { useRouter } from 'next/router'
 
 const NavbarComponent = () => {
+    const router = useRouter()
   return (
     <div className={styles.container}>
         <nav className={styles.navBar}>
-        <div className={styles.navBar_leftContainer}>
-            
+            <h1>Pro Connect</h1>
+        <div className={styles.navBar_OptionContainer}>
+            <div >
+                <p>Be a part</p>
+            </div>
+
         </div>
         </nav>
     </div>
