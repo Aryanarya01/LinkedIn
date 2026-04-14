58500a28780e297c3f030dee9e0244937e5f5498
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-14 23:25:47 +0530

    login component

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -22,7 +22,12 @@ const LoginComponent = () => {
           <div className={styles.cardContainer_left}>
             <p className={styles.cardleft_heading} >{userLoginMethod ? "Sign In" : "Sign Up"}</p>
           
-            <input type="text" placeholder="Username" className={styles.inputField} />
+            <div className={styles.inputContainers}>
+              <div>
+                 <input type="text" placeholder="Username" className={styles.inputField} />
+                  <input type="text" placeholder="Username" className={styles.inputField} />
+              </div>
+            </div>
           
           
           </div>
