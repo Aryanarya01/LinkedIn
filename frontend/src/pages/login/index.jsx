96ceba2e6e83d9c344cea0cd511340ed83884579
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-14 22:55:30 +0530

    login component

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,30 +1,26 @@
-import NavbarComponent from '@/Components/Navbar'
-import UserLayout from '@/layout/UserLayout'
-import { useRouter } from 'next/router'
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import styles from "./style.module.css"
+import NavbarComponent from "@/Components/Navbar";
+import UserLayout from "@/layout/UserLayout";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import styles from "./style.module.css";
 
 const LoginComponent = () => {
-  const authState = useSelector((state)=>state.auth)
+  const authState = useSelector((state) => state.auth);
   const router = useRouter();
-  useEffect(()=>{
-    if(authState.loggedIn){
-      router.push("/dashboard")
+  useEffect(() => {
+    if (authState.loggedIn) {
+      router.push("/dashboard");
     }
-  })
+  });
   return (
-      <UserLayout>
-        <div className={styles.cardContainer}>
-          <div className={styles.cardContainer_left}>
+    <UserLayout>
+      <div className={styles.cardContainer}>
+        <div className={styles.cardContainer_left}></div>
+        <div className={styles.cardContainer_right}></div>
+      </div>
+    </UserLayout>
+  );
+};
 
-          </div>
-          <div className={styles.cardContainer_right}>
-
-          </div>
-        </div>
-      </UserLayout> 
-  )
-}
-
-export default LoginComponent
+export default LoginComponent;
