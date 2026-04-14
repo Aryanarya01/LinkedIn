c27d4c2943f12070673479f3cf941c393a27f658
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-14 22:49:09 +0530

    login component

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,11 +1,14 @@
 import NavbarComponent from '@/Components/Navbar'
 import UserLayout from '@/layout/UserLayout'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const LoginComponent = () => {
+  const authState = useSelector((state)=>state.auth)
+
   return (
       <UserLayout>
-          
+
       </UserLayout>
   )
 }
