396dca1d4c1698bad04beaad6d338af0f27b5e25
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-15 18:10:01 +0530

    Dashbard

---
+++ frontend/.next/dev/server/pages/dashboard.js
@@ -0,0 +1,8 @@
+var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/dashboard.js")
+R.c("server/chunks/ssr/node_modules_next_dist_0_q2gfp._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__0mrwzy0._.js")
+R.c("server/chunks/ssr/node_modules_047xm.d._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__0t8l382._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__0z.fli-._.js")
+R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/dashboard/index.jsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/src/pages/_document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)")
+module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/dashboard/index.jsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/src/pages/_document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
