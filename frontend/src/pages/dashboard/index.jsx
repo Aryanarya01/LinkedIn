89ca8499e4e70962eb37b9b48d7e9098577d907d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-17 00:01:02 +0530

    dashboard scroll styling

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -33,7 +33,7 @@ const Dashboard = () => {
                   src={`${BASE_URL}/${authState.user.userId.profilePicture}`}
                   alt=""
                 />
-                <textarea className={styles.textArea} name="" id=""></textarea>
+                <textarea placeholder={"What's in your mind?"} className={styles.textArea} name="" id=""></textarea>
                 <label htmlFor="fileUpload">
                   <div className={styles.Fab}>
                     <svg
