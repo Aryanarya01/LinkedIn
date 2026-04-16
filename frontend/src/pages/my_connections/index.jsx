114d0de785dcfd82b6e22970dd6886c665f4a63a
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-16 18:50:42 +0530

    My connections Page

--- frontend/src/pages/my_connections/index.jsx
+++ frontend/src/pages/my_connections/index.jsx
@@ -1,8 +1,21 @@
+import DashboardLayout from '@/layout/DashboardLayout';
+import UserLayout from '@/layout/UserLayout';
 import React from 'react'
 
 const MyConnectionsPage = () => {
   return (
-    <div>MyConnectionsPage</div>
+     <UserLayout>
+         
+      <DashboardLayout>
+        <div>
+          <h1>
+            My Connections
+          </h1>
+        </div>
+      </DashboardLayout>
+
+
+      </UserLayout>
   )
 }
 
