c10e7a4e17fbe49323fa2a0152f5ff93b11eb8d2
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-16 18:52:11 +0530

    My connections Page

---
+++ frontend/src/pages/discover/index.jsx
@@ -0,0 +1,24 @@
+import DashboardLayout from '@/layout/DashboardLayout'
+import UserLayout from '@/layout/UserLayout'
+import React from 'react'
+
+const DiscoverPage = () => {
+  return (
+    <div>
+        <UserLayout>
+         
+      <DashboardLayout>
+        <div>
+          <h1>
+            Discover
+          </h1>
+        </div>
+      </DashboardLayout>
+
+
+      </UserLayout>
+    </div>
+  )
+}
+
+export default DiscoverPage
