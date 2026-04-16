3d83373e859241b8691c093928cb3700175ae325
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-16 23:29:48 +0530

    dashboard scroll styling

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,66 +1,71 @@
-import { getAboutUser, getAllUsers } from '@/config/redux/action/AuthAction';
-import { getAllPosts } from '@/config/redux/action/PostAction';
-import DashboardLayout from '@/layout/DashboardLayout';
-import UserLayout from '@/layout/UserLayout';
-import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import styles from "./index.module.css"
-import { BASE_URL } from '@/config';
+import { getAboutUser, getAllUsers } from "@/config/redux/action/AuthAction";
+import { getAllPosts } from "@/config/redux/action/PostAction";
+import DashboardLayout from "@/layout/DashboardLayout";
+import UserLayout from "@/layout/UserLayout";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./index.module.css";
+import { BASE_URL } from "@/config";
 const Dashboard = () => {
-    const router  = useRouter();
-    const dispath = useDispatch();
-    const authState = useSelector((state)=>state.auth)
+  const router = useRouter();
+  const dispath = useDispatch();
+  const authState = useSelector((state) => state.auth);
 
-
-  
-
-  useEffect(()=>{
-    if(authState.isTokenThere){
-      dispath(getAllPosts())
-      dispath(getAboutUser({token : localStorage.getItem('token')}))
+  useEffect(() => {
+    if (authState.isTokenThere) {
+      dispath(getAllPosts());
+      dispath(getAboutUser({ token: localStorage.getItem("token") }));
     }
-    if(!authState.all_profiles_fetched){
-      dispath(getAllUsers())
+    if (!authState.all_profiles_fetched) {
+      dispath(getAllUsers());
     }
-  },[authState.isTokenThere])
+  }, [authState.isTokenThere]);
 
-  if(authState.user){
-  return (
-   
+  if (authState.user) {
+    return (
       <UserLayout>
-         
-      <DashboardLayout>
-        <div>
-          <div className="scrollComponent">
+        <DashboardLayout>
+          <div>
+            <div className="scrollComponent">
               <div className={styles.createPostContainer}>
-                <img width={200} src={`${BASE_URL}/${authState.user.userId.profilePicture}`} alt="" />
+                <img
+                  width={200}
+                  src={`${BASE_URL}/${authState.user.userId.profilePicture}`}
+                  alt=""
+                />
                 <textarea name="" id=""></textarea>
                 <div className="Fab">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-</svg>
-
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 4.5v15m7.5-7.5h-15"
+                    />
+                  </svg>
                 </div>
               </div>
+            </div>
           </div>
-        </div>
-      </DashboardLayout>
-
-
+        </DashboardLayout>
       </UserLayout>
-  )}else{
-      return(
-         <UserLayout>
-         
-      <DashboardLayout>
-       <h2>Loading...</h2>
-      </DashboardLayout>
-
-
+    );
+  } else {
+    return (
+      <UserLayout>
+        <DashboardLayout>
+          <h2>Loading...</h2>
+        </DashboardLayout>
       </UserLayout>
-      )
+    );
   }
-}
+};
 
-export default Dashboard
+export default Dashboard;
