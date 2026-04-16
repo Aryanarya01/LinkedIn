098625f0e1344d14253a3f9f89b07934c0ac8b7c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-16 23:29:41 +0530

    dashboard scroll styling

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -35,6 +35,13 @@ const Dashboard = () => {
           <div className="scrollComponent">
               <div className={styles.createPostContainer}>
                 <img width={200} src={`${BASE_URL}/${authState.user.userId.profilePicture}`} alt="" />
+                <textarea name="" id=""></textarea>
+                <div className="Fab">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+</svg>
+
+                </div>
               </div>
           </div>
         </div>
