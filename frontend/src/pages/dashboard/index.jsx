cb4b3a4123a66720c47fb0e6a2c1bffb060ab627
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-16 23:32:34 +0530

    dashboard scroll styling fileupload

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -35,22 +35,25 @@ const Dashboard = () => {
                   alt=""
                 />
                 <textarea name="" id=""></textarea>
-                <div className="Fab">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 4.5v15m7.5-7.5h-15"
-                    />
-                  </svg>
-                </div>
+                <label htmlFor="fileUpload">
+                  <div className="Fab">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 4.5v15m7.5-7.5h-15"
+                      />
+                    </svg>
+                  </div>
+                </label>
+                <input type="file" hidden id="fileUpload" />
               </div>
             </div>
           </div>
