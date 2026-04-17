f4ee15ea3269f00b5969ca762d4644a2d8f36605
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-17 22:01:39 +0530

    wrapper

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -7,6 +7,9 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./index.module.css";
 import { BASE_URL } from "@/config";
+
+
+
 const Dashboard = () => {
   const router = useRouter();
   const dispath = useDispatch();
@@ -41,48 +44,49 @@ const Dashboard = () => {
         <DashboardLayout>
           
             <div className={styles.scrollComponent}>
-              <div className={styles.createPostContainer}>
-                <img className={styles.userProfile}
-                  src={`${BASE_URL}/${authState.user.userId.profilePicture}`}
-                  alt=""
-                />
-                <textarea onChange={(e)=>setPostContent(e.target.value)} value={postContent} placeholder={"What's in your mind?"} className={styles.textArea} name="" id=""></textarea>
-                <label htmlFor="fileUpload">
-                  <div className={styles.Fab}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 4.5v15m7.5-7.5h-15"
-                      />
-                    </svg>
-                  </div>
-                </label>
-                <input onChange={(e)=>setFileContent(e.target.files[0])} type="file" hidden id="fileUpload" />
-                {postContent.length > 0 && 
-                <div onClick={handleUpload} className={styles.uploadButton}>Post</div>
-                }
-              </div>
-
-
-                <div className={styles.postContainer}>
-                    {postState.posts.map((post)=>{
-                      return(
-                         <div key={post._id} className={styles.singleCard}> 
-
-                          <div className={styles.singleCard_profileContainer}></div>
-
-                         </div>
-                      )
-                    })}
+              <div className={styles.wrapper}>
+                <div className={styles.createPostContainer}>
+                  <img className={styles.userProfile}
+                    src={`${BASE_URL}/${authState.user.userId.profilePicture}`}
+                    alt=""
+                  />
+                  <textarea onChange={(e)=>setPostContent(e.target.value)} value={postContent} placeholder={"What's in your mind?"} className={styles.textArea} name="" id=""></textarea>
+                  <label htmlFor="fileUpload">
+                    <div className={styles.Fab}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 4.5v15m7.5-7.5h-15"
+                        />
+                      </svg>
+                    </div>
+                  </label>
+                  <input onChange={(e)=>setFileContent(e.target.files[0])} type="file" hidden id="fileUpload" />
+                  {postContent.length > 0 &&
+                  <div onClick={handleUpload} className={styles.uploadButton}>Post</div>
+                  }
                 </div>
+                  <div className={styles.postContainer}>
+                      {postState.posts.map((post)=>{
+                        return(
+                           <div key={post._id} className={styles.singleCard}>
+                            <div className={styles.singleCard_profileContainer}>
+                              <img className={styles.userProfile}  src={`${BASE_URL}/${authState.user.userId.profilePicture}`} alt="" />
+                              <p>{post.userId.name}</p>
+                            </div>
+                           </div>
+                        )
+                      })}
+                  </div>
+              </div>
 
             </div>
          
