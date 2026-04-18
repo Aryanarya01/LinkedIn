94936d755a8654f3f80fd6769c9a27caaa54d05d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-18 22:07:22 +0530

    option container styling share feature

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -154,7 +154,10 @@ const Dashboard = () => {
 
 
                                 </div>
-                                 <div className={styles.singleOption__optionContainer}>
+                                 <div onClick={()=>{
+                                  const text = encodeURIComponent(post.body)
+                                  
+                                 }} className={styles.singleOption__optionContainer}>
 
 
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
