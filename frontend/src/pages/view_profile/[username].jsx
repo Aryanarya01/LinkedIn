99f6f01c1ea6c44ac5ac1ff87a148f59bc115156
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-22 17:40:32 +0530

    view_profile

---
+++ frontend/src/pages/view_profile/[username].jsx
@@ -0,0 +1,11 @@
+import { useSearchParams } from 'next/navigation'
+import React from 'react'
+
+const ViewProfilePage = () => {
+    const searchParames = useSearchParams()
+  return (
+    <div>ViewProfilePage</div>
+  )
+}
+
+export default ViewProfilePage
