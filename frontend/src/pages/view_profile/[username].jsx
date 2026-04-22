2efb1f370a2a9ed8a39daaf292a4484ad0b18f36
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-22 18:06:26 +0530

    ssr serverside rendering

--- frontend/src/pages/view_profile/[username].jsx
+++ frontend/src/pages/view_profile/[username].jsx
@@ -1,3 +1,4 @@
+import { clientServer } from '@/config'
 import { useSearchParams } from 'next/navigation'
 import React, { useEffect } from 'react'
 
@@ -19,5 +20,9 @@ export async function getServerSideProps(context) {
     console.log("view")
     console.log(context.query.username);
     
+  const request = await clientServer.get("/user/get_profile_based_on_username",{
+    
+  })
+
     return {props: {}}
 }
