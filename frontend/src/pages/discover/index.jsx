9170ecbe3c9bb27637260055339f2b8720fd5a94
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-22 07:48:44 +0530

    Discover Page

--- frontend/src/pages/discover/index.jsx
+++ frontend/src/pages/discover/index.jsx
@@ -3,6 +3,9 @@ import DashboardLayout from '@/layout/DashboardLayout'
 import UserLayout from '@/layout/UserLayout'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import styles from "./index.module.css"
+
+
 
 const DiscoverPage = () => {
   const authState = useSelector((state)=>state.auth);
@@ -25,7 +28,15 @@ const DiscoverPage = () => {
           </h1>
 
         <div className={styles.allUserProfile}>
-          {authState.all_profiles_fetched && authState.al_}
+          {authState.all_profiles_fetched && authState.all_users.map((user)=>{
+            return(
+              <div key={user._id} className={styles.userProfile}>
+                    <img src={`${BAS}`} alt="Profile"/>
+                    <h1>{user.userId.name}</h1>
+                    <p>{user.userId.username}</p>
+              </div>
+            )
+          })}
         </div>
 
         </div>
@@ -37,4 +48,4 @@ const DiscoverPage = () => {
   )
 }
 
-export default DiscoverPage
+export default DiscoverPage;
