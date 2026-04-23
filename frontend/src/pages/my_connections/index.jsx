58d9290120db1561a86dfc67aabdb6385ecd2bb5
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-23 18:52:15 +0530

    my_connections styling

--- frontend/src/pages/my_connections/index.jsx
+++ frontend/src/pages/my_connections/index.jsx
@@ -1,61 +1,54 @@
-import { BASE_URL } from '@/config';
-import { getMyConnectionRequests } from '@/config/redux/action/AuthAction';
-import DashboardLayout from '@/layout/DashboardLayout';
-import UserLayout from '@/layout/UserLayout';
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import styles from "./index.module.css"
-
+import { BASE_URL } from "@/config";
+import { getMyConnectionRequests } from "@/config/redux/action/AuthAction";
+import DashboardLayout from "@/layout/DashboardLayout";
+import UserLayout from "@/layout/UserLayout";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./index.module.css";
 
 const MyConnectionsPage = () => {
-
   const dispatch = useDispatch();
-  const authState = useSelector((state)=>state.auth)
-  useEffect(()=>{
-    dispatch(getMyConnectionRequests({token : localStorage.getItem("token")}))
-  },[])
+  const authState = useSelector((state) => state.auth);
+  useEffect(() => {
+    dispatch(getMyConnectionRequests({ token: localStorage.getItem("token") }));
+  }, []);
 
-  useEffect(()=>{
-    if(authState.connectionRequest.length != 0){
+  useEffect(() => {
+    if (authState.connectionRequest.length != 0) {
       console.log(authState.connectionRequest);
-      
     }
-  },[authState.connectionRequest])
+  }, [authState.connectionRequest]);
 
   return (
-     <UserLayout>
-         
+    <UserLayout>
       <DashboardLayout>
         <div>
-          <h1>
-            My Connections
-          </h1>
+          <h1>My Connections</h1>
 
-          {authState.connectionRequest.length != 0 && authState.connectionRequest.map((user,index)=>{
-            return(
-               <div key={index} className={styles.userCard}>
-                <div style={{display:"flex", alignItems:"center"}}>
+          {authState.connectionRequest.length != 0 &&
+            authState.connectionRequest.map((user, index) => {
+              return (
+                <div key={index} className={styles.userCard}>
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <div className={styles.profilePicture}>
+                      <img
+                        src={`${BASE_URL}/${user.userId.profilePicture}`}
+                        alt=""
+                      />
+                    </div>
 
-              <div className={styles.profilePicture}>
-                <img src={`${BASE_URL}/${user.userId.profilePicture}`} alt="" />
-              </div>
-
-              <div className={styles.userInfo}>
-                <h3>{user.userId.name}</h3>
-                <p>{user.userId.username}</p>
-              </div>
-
+                    <div className={styles.userInfo}>
+                      <h3>{user.userId.name}</h3>
+                      <p>{user.userId.username}</p>
+                    </div>
+                  </div>
                 </div>
-               </div>
-            )
-          })}
-
+              );
+            })}
         </div>
       </DashboardLayout>
-
-
-      </UserLayout>
-  )
-}
+    </UserLayout>
+  );
+};
 
 export default MyConnectionsPage;
