e7bbc93a9d7b59526412184601a63f18e75f72e5
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-25 18:35:41 +0530

    responsiveness mobileNavbar

--- frontend/src/pages/view_profile/[username].jsx
+++ frontend/src/pages/view_profile/[username].jsx
@@ -93,7 +93,7 @@ const ViewProfilePage = ({ userProfile }) => {
           </div>
 
           <div className={styles.profileContainer_details}>
-            <div style={{ display: "flex", gap: "0.7rem" }}>
+            <div className={styles.profileContainer_flex} >
               <div style={{ flex: "0.8" }}>
                 <div
                   style={{
