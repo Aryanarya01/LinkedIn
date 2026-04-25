0193b30140b1c87599ecf22048829c4cea7c4160
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-25 18:24:29 +0530

    responsiveness mobileNavbar

--- frontend/src/layout/DashboardLayout/index.jsx
+++ frontend/src/layout/DashboardLayout/index.jsx
@@ -93,7 +93,9 @@ const DashboardLayout = ({ children }) => {
 
             <div className={styles.mobileNavbar}> 
 
-            <div className={styles.singleNavItemHolder_mobileView}>
+            <div onClick={()=>{
+                router.push("/dashboard")
+              }}  className={styles.singleNavItemHolder_mobileView}>
                <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -110,7 +112,9 @@ const DashboardLayout = ({ children }) => {
                 </svg>
             </div>
 
-            <div className={styles.singleNavItemHolder_mobileView}>
+            <div onClick={()=>{
+                router.push("/discover")
+              }} className={styles.singleNavItemHolder_mobileView}>
                <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -128,7 +132,9 @@ const DashboardLayout = ({ children }) => {
             </div>
 
 
-            <div className={styles.singleNavItemHolder_mobileView}>
+            <div onClick={()=>{
+                  router.push("/my_connections")
+                }} className={styles.singleNavItemHolder_mobileView}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
 </svg>
