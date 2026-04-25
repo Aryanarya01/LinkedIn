233dca4d7b19690697f9d9620556f9ce2de6296b
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-25 22:05:48 +0530

    implementing education section

--- frontend/src/pages/profile/index.jsx
+++ frontend/src/pages/profile/index.jsx
@@ -280,9 +280,18 @@ const ProfilePage = () => {
             <h3>Education</h3>
             <div className={styles.workHistoryContainer}>
               {userProfile.education.map((study,index)=>{
-                return(
+                return (
                   <div key={index} className={styles.workHistoryCard}>
-
+                         <p
+                          style={{
+                            fontWeight: "bold",
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "0.8rem",
+                          }}
+                        >
+                          {study.school}
+                        </p>
                   </div>
                 )
               })}
