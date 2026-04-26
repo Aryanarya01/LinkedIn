278d418385634ec0a9b3b5d21a31248c3ca00d2f
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-26 09:30:13 +0530

    LinkedIn Completed

--- frontend/src/pages/my_connections/index.jsx
+++ frontend/src/pages/my_connections/index.jsx
@@ -1,5 +1,8 @@
 import { BASE_URL } from "@/config";
-import { AcceptConnection, getMyConnectionRequests } from "@/config/redux/action/AuthAction";
+import {
+  AcceptConnection,
+  getMyConnectionRequests,
+} from "@/config/redux/action/AuthAction";
 import DashboardLayout from "@/layout/DashboardLayout";
 import UserLayout from "@/layout/UserLayout";
 import React, { useEffect } from "react";
@@ -12,9 +15,6 @@ const MyConnectionsPage = () => {
   const dispatch = useDispatch();
   const authState = useSelector((state) => state.auth);
   const router = useRouter();
-
-
-
 
   useEffect(() => {
     dispatch(getMyConnectionRequests({ token: localStorage.getItem("token") }));
@@ -29,18 +29,87 @@ const MyConnectionsPage = () => {
   return (
     <UserLayout>
       <DashboardLayout>
-        <div style={{display : "flex", flexDirection:"column", gap:"1.7rem"}}>
+        <div
+          style={{ display: "flex", flexDirection: "column", gap: "1.7rem" }}
+        >
           <h4>My Connections</h4>
 
-          {authState.connectionRequest.length === 0 && <h1>No Connection Request Pending</h1>}
+          {authState.connectionRequest.length === 0 && (
+            <h1>No Connection Request Pending</h1>
+          )}
 
           {authState.connectionRequest.length != 0 &&
-            authState.connectionRequest.filter((connection)=>connection.status_accepted === null).map((user, index) => {
+            authState.connectionRequest
+              .filter((connection) => connection.status_accepted === null)
+              .map((user, index) => {
+                return (
+                  <div key={index} className={styles.userCard}>
+                    <div
+                      onClick={() => {
+                        router.push(`/view_profile/${user.userId.username}`);
+                      }}
+                      style={{
+                        display: "flex",
+                        cursor: "pointer",
+                        alignItems: "center",
+                        gap: "1.2rem",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <div className={styles.profilePicture}>
+                        <img
+                          src={`${BASE_URL}/${user.userId.profilePicture}`}
+                          alt=""
+                        />
+                      </div>
+
+                      <div className={styles.userInfo}>
+                        <h3>{user.userId.name}</h3>
+                        <p>{user.userId.username}</p>
+                      </div>
+                      <button
+                        onClick={async (e) => {
+                          e.stopPropagation();
+                          await dispatch(
+                            AcceptConnection({
+                              token: localStorage.getItem("token"),
+                              connectionId: user._id,
+                              action_type: "accept",
+                            }),
+                          );
+                          dispatch(
+                            getMyConnectionRequests({
+                              token: localStorage.getItem("token"),
+                            }),
+                          );
+                        }}
+                        className={styles.connectedButton}
+                      >
+                        Accept
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+
+          <h4>My Network</h4>
+          {authState.connectionRequest
+            .filter((connection) => connection.status_accepted !== null)
+            .map((user, index) => {
               return (
                 <div key={index} className={styles.userCard}>
-                  <div onClick={()=>{
-                    router.push(`/view_profile/${user.userId.username}`)
-                  }} style={{ display: "flex",cursor:"pointer", alignItems: "center",gap:"1.2rem",justifyContent:"space-between" }}>
+                  <div
+                    onClick={() => {
+                      router.push(`/view_profile/${user.userId.username}`);
+                    }}
+                    style={{
+                      display: "flex",
+                      cursor: "pointer",
+                      alignItems: "center",
+                      gap: "1.2rem",
+                      justifyContent: "space-between",
+                    }}
+                  >
                     <div className={styles.profilePicture}>
                       <img
                         src={`${BASE_URL}/${user.userId.profilePicture}`}
@@ -52,48 +121,10 @@ const MyConnectionsPage = () => {
                       <h3>{user.userId.name}</h3>
                       <p>{user.userId.username}</p>
                     </div>
-                     <button onClick={async (e)=>{
-                      e.stopPropagation()
-                    await dispatch(AcceptConnection({token : localStorage.getItem("token"),
-                        connectionId : user._id,
-                        action_type : "accept"
-                      }))
-                       dispatch(getMyConnectionRequests({
-      token: localStorage.getItem("token")
-    }))
- 
-                     }} className={styles.connectedButton}>Accept</button>
                   </div>
                 </div>
               );
             })}
-
-
-
-                <h4>My Network</h4>
-              { authState.connectionRequest.filter((connection)=>connection.status_accepted !== null).map((user,index)=>{
-                return(
-                   <div key={index} className={styles.userCard}>
-                  <div onClick={()=>{
-                    router.push(`/view_profile/${user.userId.username}`)
-                  }} style={{ display: "flex",cursor:"pointer", alignItems: "center",gap:"1.2rem",justifyContent:"space-between" }}>
-                    <div className={styles.profilePicture}>
-                      <img
-                        src={`${BASE_URL}/${user.userId.profilePicture}`}
-                        alt=""
-                      />
-                    </div>
-
-                    <div className={styles.userInfo}>
-                      <h3>{user.userId.name}</h3>
-                      <p>{user.userId.username}</p>
-                    </div>
-                      
-                  </div>
-                </div>
-              )})}
-
-
         </div>
       </DashboardLayout>
     </UserLayout>
