ddcf02e9441573564dd5a978655dfa3daccc80d3
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-26 09:30:19 +0530

    LinkedIn Completed

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -11,80 +11,105 @@ const LoginComponent = () => {
   const authState = useSelector((state) => state.auth);
   const router = useRouter();
   const dispatch = useDispatch();
-  const [userLoginMethod,setUserLoginMethod] = useState(false);
+  const [userLoginMethod, setUserLoginMethod] = useState(false);
 
-  const [email,setEmail] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [username,setUsername] = useState("");
-  const [name,setName] = useState("")
-
-
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
 
   useEffect(() => {
     if (authState.loggedIn) {
       router.push("/dashboard");
     }
-  },[authState.loggedIn]);
-  useEffect(()=>{
-    if(localStorage.getItem("token")){
-      router.push("/dashboard")
+  }, [authState.loggedIn]);
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      router.push("/dashboard");
     }
-  })
-  useEffect(()=>{
-      dispatch(emptyMessage());
-  },[userLoginMethod])
+  });
+  useEffect(() => {
+    dispatch(emptyMessage());
+  }, [userLoginMethod]);
 
-  const handelRegister = ()=>{
+  const handelRegister = () => {
     console.log("registering");
-    dispatch(registerUser({username,password,email,name}))
-    
-  }
-  const handelLogin = ()=>{
-    console.log("login")
-    dispatch(loginUser({email,password}))
-  }
+    dispatch(registerUser({ username, password, email, name }));
+  };
+  const handelLogin = () => {
+    console.log("login");
+    dispatch(loginUser({ email, password }));
+  };
   return (
     <UserLayout>
       <div className={styles.container}>
         <div className={styles.cardContainer}>
           <div className={styles.cardContainer_left}>
-            <p className={styles.cardleft_heading} >{userLoginMethod ? "Sign In" : "Sign Up"}</p>
-            <p style={{color : authState.isError ? "red" : "green"}}>{authState.message?.message}</p>
+            <p className={styles.cardleft_heading}>
+              {userLoginMethod ? "Sign In" : "Sign Up"}
+            </p>
+            <p style={{ color: authState.isError ? "red" : "green" }}>
+              {authState.message?.message}
+            </p>
             <div className={styles.inputContainers}>
-              {
-                !userLoginMethod &&  
+              {!userLoginMethod && (
                 <div className={styles.inputRow}>
-                 <input onChange={(e)=>setUsername(e.target.value)} type="text" placeholder="Username" className={styles.inputField} />
-                  <input onChange={(e)=>setName(e.target.value)} type="text" placeholder="Name" className={styles.inputField} />
-              </div>
-              }
+                  <input
+                    onChange={(e) => setUsername(e.target.value)}
+                    type="text"
+                    placeholder="Username"
+                    className={styles.inputField}
+                  />
+                  <input
+                    onChange={(e) => setName(e.target.value)}
+                    type="text"
+                    placeholder="Name"
+                    className={styles.inputField}
+                  />
+                </div>
+              )}
 
-               <input onChange={(e)=>setEmail(e.target.value)} type="Email" placeholder="Email" className={styles.inputField} />
-                <input onChange={(e)=>setPassword(e.target.value)} type="Password" placeholder="Password" className={styles.inputField} />
-              <div onClick={()=>{
-                if(userLoginMethod){
-                  handelLogin()
-                }else{
-                  handelRegister()
-                }
-              }} className={styles.buttonWithOutline}>
-                  <p>{userLoginMethod ? "Sign In" : "Sign Up"}</p>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                type="Email"
+                placeholder="Email"
+                className={styles.inputField}
+              />
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                type="Password"
+                placeholder="Password"
+                className={styles.inputField}
+              />
+              <div
+                onClick={() => {
+                  if (userLoginMethod) {
+                    handelLogin();
+                  } else {
+                    handelRegister();
+                  }
+                }}
+                className={styles.buttonWithOutline}
+              >
+                <p>{userLoginMethod ? "Sign In" : "Sign Up"}</p>
               </div>
-            
             </div>
-          
-          
           </div>
           <div className={styles.cardContainer_right}>
-
-        
-                {userLoginMethod ? <p>Don't Have an Account?</p> :<p>Already Have an Account?</p>}
-                   <div onClick={()=>{
-                      setUserLoginMethod(!userLoginMethod)
-                }} style={{color:"black",textAlign : "center"}} className={styles.buttonWithOutline}>
-                    <p>{userLoginMethod ? "Sign Up" : "Sign In"}</p>
-                </div>
-               
+            {userLoginMethod ? (
+              <p>Don't Have an Account?</p>
+            ) : (
+              <p>Already Have an Account?</p>
+            )}
+            <div
+              onClick={() => {
+                setUserLoginMethod(!userLoginMethod);
+              }}
+              style={{ color: "black", textAlign: "center" }}
+              className={styles.buttonWithOutline}
+            >
+              <p>{userLoginMethod ? "Sign Up" : "Sign In"}</p>
+            </div>
           </div>
         </div>
       </div>
