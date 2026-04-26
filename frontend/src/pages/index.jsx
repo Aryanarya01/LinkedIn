7b39b783f09c13770a3cc4b510be1d3ea1eed170
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-04-26 09:31:26 +0530

    LinkedIn Completed

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -15,29 +15,30 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
-
-const router = useRouter();
+  const router = useRouter();
 
   return (
     <>
       <NavbarComponent />
-        <div className={styles.container}>
-          <div className={styles.mainContainer}>
-            <div className={styles.mainContainer_left}>
-              <p>Connect with Friends without Exaggerations</p>
-              <p>A True social media platform, with stories no blufs!</p>
-              <div onClick={()=>{
-                router.push("/login")
-              }} className={styles.buttonJoin}>
-                <p>Join Now</p>
-              </div>
-            </div>
-            <div className={styles.mainContainer_right}>
-              <img src="images/Connection.png"/>
+      <div className={styles.container}>
+        <div className={styles.mainContainer}>
+          <div className={styles.mainContainer_left}>
+            <p>Connect with Friends without Exaggerations</p>
+            <p>A True social media platform, with stories no blufs!</p>
+            <div
+              onClick={() => {
+                router.push("/login");
+              }}
+              className={styles.buttonJoin}
+            >
+              <p>Join Now</p>
             </div>
           </div>
+          <div className={styles.mainContainer_right}>
+            <img src="images/Connection.png" />
+          </div>
         </div>
-    
+      </div>
     </>
   );
 }
